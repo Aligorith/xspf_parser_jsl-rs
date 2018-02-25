134b8e32ac16c8db013fcaad272cfb479bce31d5
Joshua Leung <aligorith@gmail.com>
2018-02-25 23:24:04 +1300

    WIP - Non-compiling attempt to clean up the argument handling
    Unfortunately, there's a mismatch here between String and str types...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #[macro_use] extern crate indoc;
+#[macro_use] mod logic_macros;
 
 use std::env;
 
 //mod xspf_parser;
-#[macro_use] mod logic_macros;
 
 fn print_usage_info()
 {
@@ -23,6 +23,7 @@ fn print_usage_info()
 fn list_output_mode(in_file: &str, out_file: Option<&String>)
 {
 	println!("List in='{0}', out={1:?}", in_file, out_file);
+	//let xspf = xspf_parser::parse_xspf(in_file); 
 }
 
 fn json_output_mode(in_file: &str, out_file: Option<&String>)
@@ -34,28 +35,44 @@ fn main()
 {
 	let args: Vec<String> = env::args().collect();
 	
-	if (args.len() > 2) && (&args[1] != "help") {
-		let in_file = &args[2];
-		let out_file = if args.len() == 4 { Some(&args[3]) } else { None };
+	match args.get(1) {
+		Some("list") => {
+			let in_file = args.get(2);
+			let out_file = args.get(3);
+			
+			match in_file {
+				Some(f) => {
+					list_output_mode(f, out_file);
+				},
+				None => {
+					println!("ERROR: You need to supply a .xspf filename as the second argument!\n");
+					print_usage_info();
+				}
+			}
+		},
 		
-		match args[1].as_ref() {
-			"list" => {
-				list_output_mode(&in_file, out_file);
-			},
-			"json" => {
-				json_output_mode(&in_file, out_file);
-			},
-			arg => {
-				println!("Unrecognised option: '{0}'", arg);
-				print_usage_info();
+		Some("json") => {
+			let in_file = args.get(2);
+			let out_file = args.get(3);
+			
+			match in_file {
+				Some(f) => {
+					json_output_mode(f, out_file);
+				},
+				None => {
+					println!("ERROR: You need to supply a .xspf filename as the second argument!\n");
+					print_usage_info();
+				}
 			}
 		}
-	}
-	else if (args.len() > 1) && elem!(&args[1], "list", "json") {
-		println!("ERROR: You need to supply a .xspf filename as the second argument!\n");
-		print_usage_info();
-	}
-	else {
-		print_usage_info();
+		
+		Some("help") | None => {
+			print_usage_info();
+		},
+		
+		Some(arg) => {
+			println!("Unrecognised option: '{0:?}'", arg);
+			print_usage_info();
+		}
 	}
 }
