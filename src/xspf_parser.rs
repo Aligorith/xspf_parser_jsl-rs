3cbca243240e292c74c9124a7ca141cda0293281
Joshua Leung <aligorith@gmail.com>
2020-10-12 01:07:23 +1300

    Fix: Use "urlparse" crate to unencode the Unicode characters in the URL's
    This is necessary when dealing with CJK characters in filenames too, as there are now too many
    to manually encode hacks for.

--- src/xspf_parser.rs
+++ src/xspf_parser.rs
@@ -6,6 +6,9 @@
 extern crate minidom;
 use self::minidom::Element;
 
+extern crate urlparse;
+use self::urlparse::unquote;
+
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -40,26 +43,8 @@ impl Track {
 	/* Generate a track element from a file path */
 	pub fn from_filepath(path: &str) -> Result<Track, &'static str>
 	{
-		/* full "unmodfied" path (with the symbols replaced, so that we can find the files) */
-		// TODO: Replace these hardcoded cases for something based on an encoding library (e.g. encoding_rs)
-		let fullpath = path.to_string()
-		                   .replace("%20", " ")
-		                   .replace("%21", "!")
-		                   .replace("%26", "&")
-		                   .replace("%27", "'")
-		                   .replace("%28", "(")
-		                   .replace("%29", ")")
-		                   .replace("%5B", "[")
-		                   .replace("%5D", "]")
-		                   .replace("%2C", ",")
-		                   .replace("%C3%A8", "è")
-		                   .replace("%C3%A9", "é")
-		                   .replace("%C3%AD", "í")
-		                   .replace("%C3%BA", "ú")
-		                   .replace("%E2%80%9C", "“")
-		                   .replace("%E2%80%9D", "”")
-		                   .replace("%E2%80%99", "’")
-		                   ;
+		/* Full "unmodfied" path (with the symbols replaced, so that we can find the files) */
+		let fullpath = unquote(path).ok().unwrap();
 		
 		/* extra filename and date from the last parts of the path 
 		 * WARNING: We're extracting these in reverse order! So first filename, then date!
